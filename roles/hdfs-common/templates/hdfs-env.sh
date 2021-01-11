6a7a25c456ae753121a568d0ee265fdf184d5e19
shenlan <xz@onwalk.net>
2021-01-11 16:47:09 +0800

    set JAVA_HOME=/usr/lib/jvm/jre-11-openjdk

--- roles/hdfs-common/templates/hdfs-env.sh
+++ roles/hdfs-common/templates/hdfs-env.sh
@@ -2,7 +2,7 @@ export HDFS_NAMENODE_USER=root
 export HDFS_DATANODE_USER=root
 export HDFS_SECONDARYNAMENODE_USER=root
 
-export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-11.0.9.11-2.el8_3.x86_64/
+export JAVA_HOME=/usr/lib/jvm/jre-11-openjdk
 export PATH=$JAVA_HOME/bin:$PATH
 export CLASSPATH=.:$JAVA_HOME/lib/dt.jar:$JAVA_HOME/lib/tools.jar
 export PATH=$PATH:/data/hadoop-3.3.0/bin/:/data/hadoop-3.3.0/sbin/
